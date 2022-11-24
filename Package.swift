7604d4156903e0e34d0affc2f2f55ce65e7f8151
Nathan <Nathan-nguyen@outlook.com>
2022-11-23 23:48:41 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,7 +18,8 @@ let package = Package(
     targets: [
         .target(
             name: "AFNetworking+RetryPolicy",
-            dependencies: [.product(name: "AFNetworking", package: "AFNetworking"),        )
+            dependencies: [.product(name: "AFNetworking", package: "AFNetworking")
+                          )
     ],
     swiftLanguageVersions: [.v5]
 )
