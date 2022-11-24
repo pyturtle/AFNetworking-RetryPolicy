600ef23a6295d6aa2ae8816214c34a3f99b99b7c
Nathan <Nathan-nguyen@outlook.com>
2022-11-24 00:42:47 -0500

    Update AFHTTPSessionManager+RetryPolicy.m

--- Sources/AFNetworking+RetryPolicy/AFHTTPSessionManager+RetryPolicy.m
+++ Sources/AFNetworking+RetryPolicy/AFHTTPSessionManager+RetryPolicy.m
@@ -10,7 +10,7 @@
 // - Copyright (c) 2016 Jakub Truhlar. All rights reserved.
 //
 
-#import <Sources/ObjcAssociatedObjectHelpers/ObjcAssociatedObjectHelpers.h>
+#import <ObjcAssociatedObjectHelpers.h>
 
 #import "AFHTTPSessionManager+RetryPolicy.h"
 
