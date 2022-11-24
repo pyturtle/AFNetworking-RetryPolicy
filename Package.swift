da60f5e33604fb813ce90fdca708f6647adc4bdf
Nathan <Nathan-nguyen@outlook.com>
2022-11-23 22:38:21 -0500

    Create Package.swift
    Added swift addon

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "AFNetworking+RetryPolicy",
+    platforms: [
+          .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "AFNetworking+RetryPolicy",
+            targets: ["AFNetworking+RetryPolicy"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AFNetworking/AFNetworking", from: "4.0.0"),
+        .package(url: "https://github.com/itsthejb/ObjcAssociatedObjectHelpers", from: "2.0.1")
+    ],
+    targets: [
+        .target(
+            name: "AFNetworking+RetryPolicy",
+            dependencies: [.product(name: "AFNetworking", package: "AFNetworking"),
+                           .product(name: "ObjcAssociatedObjectHelpers", package: "ObjcAssociatedObjectHelpers")]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
