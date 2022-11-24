e6ad831824e2cf639d70485c810553f4ed151b76
Nathan <Nathan-nguyen@outlook.com>
2022-11-24 00:29:09 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/AFNetworking/AFNetworking", from: "4.0.0"),
-        .package(url: "https://github.com/pyturtle/ObjcAssociatedObjectHelpers", from: "2.0.1")
+        .package(url: "https://github.com/pyturtle/ObjcAssociatedObjectHelpers", from: "1.0.0")
         
     ],
     targets: [
