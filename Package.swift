c1c5dc63d28a42da4afc4112505c21d98c708456
Nathan <Nathan-nguyen@outlook.com>
2022-11-24 00:18:08 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,12 +13,15 @@ let package = Package(
             targets: ["AFNetworking+RetryPolicy"])
     ],
     dependencies: [
-        .package(url: "https://github.com/AFNetworking/AFNetworking", from: "4.0.0")
+        .package(url: "https://github.com/AFNetworking/AFNetworking", from: "4.0.0"),
+        .package(url: "https://github.com/pyturtle/ObjcAssociatedObjectHelpers", from: "2.0.1")
+        
     ],
     targets: [
         .target(
             name: "AFNetworking+RetryPolicy",
-            dependencies: [.product(name: "AFNetworking", package: "AFNetworking")]
+            dependencies: [.product(name: "AFNetworking", package: "AFNetworking"),
+                           .product(name: "ObjcAssociatedObjectHelpers", package: "ObjcAssociatedObjectHelpers")]
         )
     ],
     swiftLanguageVersions: [.v5]
